ce98adebf04e50598395cbfbf7d337b7df977d2e
LamanMammadly <lamanmammadly@gmail.com>
2024-11-02 16:54:00 +0400

    config database

--- src/Services/Job/JobUser/Job.API/Program.cs
+++ src/Services/Job/JobUser/Job.API/Program.cs
@@ -1,3 +1,8 @@
+
+using Job.DAL.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 namespace Job.API
 {
     public class Program
@@ -5,17 +10,39 @@ namespace Job.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
 
             // Add services to the container.
 
             builder.Services.AddControllers();
 
+            // Add Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Job.API", Version = "v1" });
+            });
+
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(builder.Configuration["ConnectionStrings:Default"]);
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
-            app.UseAuthorization();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Job.API v1");
+                    c.RoutePrefix = string.Empty;
+                });
+            }
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
--- src/Services/Job/JobUser/Job.DAL/Contexts/AppDbContext.cs
+++ src/Services/Job/JobUser/Job.DAL/Contexts/AppDbContext.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Job.DAL.Contexts
 {
@@ -20,3 +17,4 @@ namespace Job.DAL.Contexts
         }
     }
 }
+
