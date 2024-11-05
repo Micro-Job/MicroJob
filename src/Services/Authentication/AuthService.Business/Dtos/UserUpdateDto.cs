726be01f417e1cb515001575cf3a6859ed0f99eb
melekhaciahmedova <melekkerimli111@gmail.com>
2024-11-05 17:03:20 +0400

    added user update validator

--- src/Services/Authentication/AuthService.Business/Dtos/UserUpdateDto.cs
+++ src/Services/Authentication/AuthService.Business/Dtos/UserUpdateDto.cs
@@ -1,4 +1,6 @@
-﻿namespace AuthService.Business.Dtos
+﻿using FluentValidation;
+
+namespace AuthService.Business.Dtos
 {
     public class UserUpdateDto
     {
@@ -8,5 +10,36 @@
         public string Email { get; set; }
         public string MainPhoneNumber { get; set; }
 
+    }
+
+    public class UserUpdateDtoValidator : AbstractValidator<UserUpdateDto>
+    {
+        public UserUpdateDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                 .NotNull()
+                 .NotEmpty()
+                 .WithMessage("Boş ola bilməz")
+                 .Length(1, 32)
+                 .WithMessage("Uzunluq 1-50 arasında olmalıdır");
+
+            RuleFor(x => x.LastName)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Boş ola bilməz")
+                .Length(1, 32)
+                .WithMessage("Uzunluq 1-50 arasında olmalıdır");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Boş ola bilməz")
+                .EmailAddress()
+                .WithMessage("E-mail doğru formatda deyil");
+
+            RuleFor(x => x.MainPhoneNumber)
+                .Matches(@"^(?:\+\d{1,3})?\d{1,4}\d{7,10}$")
+                .WithMessage("Telefon nömrəsi doğru formatda deyil");
+        }
     }
 }
