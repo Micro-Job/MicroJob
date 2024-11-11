ebec029e02f3c1e8e9d327850564ce1ee12879bc
Brazencobra <elxan.memmedli2003@gmail.com>
2024-11-11 19:05:29 +0400

    CreatedVacancyEntity

---
+++ src/Services/Job/JobUser/Job.Core/Entities/SavedVacancy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job.Core.Entities
+{
+    public class SavedVacancy : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public Guid VacancyId { get; set; }
+    }
+}
---
+++ src/Shared/Enums/WorkType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Enums
+{
+    public enum WorkType : byte
+    {
+        FullTime = 1,
+        PartTime,
+    }
+}
