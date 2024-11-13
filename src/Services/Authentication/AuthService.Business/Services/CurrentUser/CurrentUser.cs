f8e50b8a06a40089f5455566ee20f3da2abe8b64
melekhaciahmedova <melekkerimli111@gmail.com>
2024-11-13 13:06:28 +0400

    .

--- src/Services/Authentication/AuthService.Business/Services/CurrentUser/CurrentUser.cs
+++ src/Services/Authentication/AuthService.Business/Services/CurrentUser/CurrentUser.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace AuthService.Business.Services.CurrentUser
 {
