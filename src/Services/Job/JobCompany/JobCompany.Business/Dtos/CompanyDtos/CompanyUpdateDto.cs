a5db287c5afbc1c1bc4de44a897349abb0e14cb7
LamanMammadly <lamanmammadly@gmail.com>
2024-11-15 13:55:38 +0400

    CompanyUpdateDtoSomeBugFixes

--- src/Services/Job/JobCompany/JobCompany.Business/Dtos/CompanyDtos/CompanyUpdateDto.cs
+++ src/Services/Job/JobCompany/JobCompany.Business/Dtos/CompanyDtos/CompanyUpdateDto.cs
@@ -24,20 +24,20 @@ namespace JobCompany.Business.Dtos.CompanyDtos
             RuleFor(x => x.CompanyName)
                 .NotEmpty().WithMessage("Company name is required.")
                 .MaximumLength(100).WithMessage("Company name must not exceed 100 characters.")
-                .When(x => x.EmployeeCount.HasValue);
+                .When(x => x.CompanyName != null);
 
             RuleFor(x => x.CompanyInformation)
                 .MaximumLength(500).WithMessage("Company information must not exceed 500 characters.")
-                .When(x => x.EmployeeCount.HasValue);
+                .When(x => x.CompanyInformation != null);
 
             RuleFor(x => x.CompanyLocation)
                 .MaximumLength(200).WithMessage("Company location must not exceed 200 characters.")
-                .When(x => x.EmployeeCount.HasValue);
+                .When(x => x.CompanyLocation != null);
 
             RuleFor(x => x.WebLink)
                 .Matches(@"^(http|https)://[^\s/$.?#].[^\s]*$")
                 .WithMessage("WebLink must be a valid URL.")
-                .When(x => x.EmployeeCount.HasValue);
+                .When(x => x.WebLink != null);
 
             RuleFor(x => x.EmployeeCount)
                 .GreaterThanOrEqualTo(0).WithMessage("Employee count must be at least 0")
