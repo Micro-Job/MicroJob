0b6e3ad175f55b8b0f5ca712781237b50b00c4eb
Brazencobra <elxan.memmedli2003@gmail.com>
2024-11-22 12:41:52 +0400

    UpdatedMigrationToJobUser

--- src/Services/Job/JobUser/Job.DAL/Migrations/20241122081335_AddedUserStatusToUserTable.Designer.cs
+++ src/Services/Job/JobUser/Job.DAL/Migrations/20241122081335_AddedUserStatusToUserTable.Designer.cs
@@ -4,6 +4,7 @@ using Job.DAL.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Job.DAL.Migrations
 {
     [DbContext(typeof(JobDbContext))]
-    partial class JobDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241122081335_AddedUserStatusToUserTable")]
+    partial class AddedUserStatusToUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Services/Job/JobUser/Job.DAL/Migrations/20241122081335_AddedUserStatusToUserTable.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Job.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserStatusToUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResumeSkills",
+                columns: table => new
+                {
+                    ResumeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SkillId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResumeSkills", x => new { x.ResumeId, x.SkillId });
+                    table.ForeignKey(
+                        name: "FK_ResumeSkills_Resumes_ResumeId",
+                        column: x => x.ResumeId,
+                        principalTable: "Resumes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ResumeSkills_Skills_SkillId",
+                        column: x => x.SkillId,
+                        principalTable: "Skills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResumeSkills_SkillId",
+                table: "ResumeSkills",
+                column: "SkillId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ResumeSkills");
+
+            migrationBuilder.DropTable(
+                name: "Skills");
+        }
+    }
+}
