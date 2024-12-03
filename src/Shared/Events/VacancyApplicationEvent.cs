79722fece34e0c5fc7f1a0123d257c22df4ff3f9
LamanMammadly <lamanmammadly@gmail.com>
2024-12-03 16:45:21 +0400

    AddVacancyApplicationEventAndService

--- src/Shared/Events/VacancyApplicationEvent.cs
+++ src/Shared/Events/VacancyApplicationEvent.cs
@@ -8,6 +8,7 @@ namespace Shared.Events
     public class VacancyApplicationEvent
     {
         public Guid UserId { get; set; }
+        public Guid VacancyId { get; set; }
         public string Content { get; set; }
     }
 }
