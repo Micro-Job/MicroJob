0e4ab1101c8d131fe9e3a99a0b87658a46f42793
kamilaghayev <kamilizundef@gmail.com>
2024-12-05 10:50:37 +0400

    gateway update for user

--- src/Services/Job/JobCompany/JobCompany.API/Program.cs
+++ src/Services/Job/JobCompany/JobCompany.API/Program.cs
@@ -16,6 +16,7 @@ namespace JobCompany.API
             var builder = WebApplication.CreateBuilder(args);
 
 
+
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(opt =>
