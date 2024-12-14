7d2c7c51b25c89442a7bdbd630cfd8ab132ba44f
melekhaciahmedova <melekkerimli111@gmail.com>
2024-12-14 16:33:36 +0400

    UpdatedAppsettingsjson

---
+++ src/Services/Job/JobUser/Job.DAL/Migrations/20241214123242_userresumerelation.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Job.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class userresumerelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Resumes_UserId",
+                table: "Resumes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resumes_UserId",
+                table: "Resumes",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Resumes_UserId",
+                table: "Resumes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resumes_UserId",
+                table: "Resumes",
+                column: "UserId");
+        }
+    }
+}
