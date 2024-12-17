6a7661b9f2c9c8bf8450407abb1b21767cf50cb4
Baylar Sadigov <bylrsadiqov@gmail.com>
2024-12-17 12:01:03 +0400

    AddedCheckCompanyConsumerAndRefactoringService

---
+++ src/Shared/Requests/CheckCompanyRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace SharedLibrary.Requests;
+
+public class CheckCompanyRequest
+{
+    public Guid CompanyId { get; set; }
+}
---
+++ src/Shared/Responses/CheckCompanyResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace SharedLibrary.Responses;
+
+public class CheckCompanyResponse
+{
+    public bool IsExist { get; set; }
+}
