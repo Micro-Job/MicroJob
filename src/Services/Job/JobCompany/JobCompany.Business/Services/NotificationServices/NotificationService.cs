a759f1d5c89f3e2a5701bc4e4ff0ea8a65b73171
Baylar Sadigov <bylrsadiqov@gmail.com>
2024-12-19 13:33:49 +0400

    Updated Notification service

--- src/Services/Job/JobCompany/JobCompany.Business/Services/NotificationServices/NotificationService.cs
+++ src/Services/Job/JobCompany/JobCompany.Business/Services/NotificationServices/NotificationService.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using JobCompany.Business.Dtos.NotificationDtos;
 using JobCompany.Business.Exceptions.UserExceptions;
 using JobCompany.Core.Entites;
 using JobCompany.DAL.Contexts;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SharedLibrary.Exceptions;
 
 namespace JobCompany.Business.Services.NotificationServices
@@ -25,22 +27,38 @@ namespace JobCompany.Business.Services.NotificationServices
             _contextAccessor = contextAccessor;
             userGuid = Guid.Parse(_contextAccessor.HttpContext.User.FindFirst(ClaimTypes.Sid)?.Value ?? throw new UserIsNotLoggedInException());
         }
-        public async Task<List<Notification>> GetUserNotificationsAsync()
+        public async Task<List<NotificationDto>> GetUserNotificationsAsync()
         {
+            var company = await _context.Companies.FirstOrDefaultAsync(x => x.UserId == userGuid)
+                ?? throw new NotFoundException<Company>();
+
             var notifications = await _context.Notifications
-                .Where(n => n.ReceiverId == userGuid)
+                .Where(n => n.ReceiverId == company.Id)
                 .OrderByDescending(n => n.CreatedDate)
+                .Select(n => new NotificationDto
+                {
+                    Id = n.Id,
+                    ReceiverId = n.ReceiverId,
+                    SenderId = n.SenderId,
+                    CreatedDate = n.CreatedDate,
+                    Content = n.Content,
+                    IsSeen = n.IsSeen
+                })
                 .ToListAsync();
+
             return notifications;
         }
 
         public async Task MarkNotificationAsReadAsync(Guid id)
         {
-            var notification = await _context.Notifications.FindAsync(id)
-            ?? throw new NotFoundException<Notification>();
+            var company = await _context.Companies.FirstOrDefaultAsync(x => x.UserId == userGuid)
+                ?? throw new NotFoundException<Company>();
+
+            var notification = await _context.Notifications.FirstOrDefaultAsync(x => x.Id == id && x.ReceiverId == company.Id)
+                ?? throw new NotFoundException<Notification>();
 
             notification.IsSeen = true;
-            _context.Notifications.Update(notification);
+
             await _context.SaveChangesAsync();
         }
     }
