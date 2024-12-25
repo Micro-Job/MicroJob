55261d7872d0276e1d9489a274ad679df882ab8d
LamanMammadly <lamanmammadly@gmail.com>
2024-12-25 11:45:00 +0400

    RemoveComments

--- src/Services/Job/JobUser/Job.API/Program.cs
+++ src/Services/Job/JobUser/Job.API/Program.cs
@@ -65,27 +65,21 @@ namespace Job.API
                 opt.RegisterValidatorsFromAssemblyContaining<ResumeService>();
             });
 
-            // MassTransit ve RabbitMQ yapılandırması
             builder.Services.AddMassTransit(x =>
             {
-                // İlk Consumer'ı ekle
                 x.AddConsumer<VacancyCreatedConsumer>();
 
-                // İkinci Consumer'ı ekle
-                x.AddConsumer<UpdateUserApplicationStatusConsumer>(); // Yeni consumer
+                x.AddConsumer<UpdateUserApplicationStatusConsumer>(); 
 
-                // RabbitMQ yapılandırması
                 x.UsingRabbitMq((context, cfg) =>
                 {
                     cfg.Host(configuration["RabbitMQ:ConnectionString"]);
 
-                    // İlk Queue: vacancy-created-queue
                     cfg.ReceiveEndpoint("vacancy-created-queue", e =>
                     {
                         e.ConfigureConsumer<VacancyCreatedConsumer>(context);
                     });
 
-                    // İkinci Queue: user-notification-queue
                     cfg.ReceiveEndpoint("user-notification-queue", e =>
                     {
                         e.ConfigureConsumer<UpdateUserApplicationStatusConsumer>(context); // Yeni consumer
