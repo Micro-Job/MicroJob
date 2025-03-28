194ba4599da19d7a10c32edb76771e79a9883db2
Baylar Sadigov <bylrsadiqov@gmail.com>
2025-03-28 14:29:06 +0400

    Added endpoint to retrieve current user's company info

---
+++ src/Services/Job/JobCompany/JobCompany.Business/Dtos/CompanyDtos/CompanyProfileDto.cs
@@ -0,0 +1,20 @@
+﻿using JobCompany.Business.Dtos.NumberDtos;
+
+namespace JobCompany.Business.Dtos.CompanyDtos;
+
+public class CompanyProfileDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string? CompanyLogo { get; set; }
+    public string? Information { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public int? EmployeeCount { get; set; }
+    public string? Category { get; set; }
+    public ICollection<CompanyNumberDto>? CompanyNumbers { get; set; }
+    public string? Email { get; set; }
+    public string? Country { get; set; }
+    public string? City { get; set; }
+    public string? Location { get; set; }
+    public string? WebLink { get; set; }
+}
