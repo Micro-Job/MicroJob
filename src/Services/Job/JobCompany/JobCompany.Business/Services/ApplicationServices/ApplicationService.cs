494ccbdc8f79772bc9eaf37ae438d3d367ebcd47
LamanMammadly <lamanmammadly@gmail.com>
2025-04-02 16:30:56 +0400

    fixConlfict

--- src/Services/Job/JobCompany/JobCompany.Business/Services/ApplicationServices/ApplicationService.cs
+++ src/Services/Job/JobCompany/JobCompany.Business/Services/ApplicationServices/ApplicationService.cs
@@ -238,7 +238,7 @@ namespace JobCompany.Business.Services.ApplicationServices
                         Status = a.Status.StatusEnum,
                         VacancyId = a.VacancyId,
                         VacancyName = a.Vacancy.Title,
-                        ProfileImage = user.ProfileImage
+                        ProfileImage = user.ProfileImage,
                         DateTime = a.CreatedDate
                     };
                 })
