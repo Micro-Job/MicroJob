86a565d1e7837199a12eff819427d40a4ad95f15
Brazencobra <elxan.memmedli2003@gmail.com>
2025-04-03 17:01:54 +0400

    CreatedPaymentMicroservice

---
+++ src/Services/JobPayment/JobPayment.Core/Entities/Balance.cs
@@ -0,0 +1,16 @@
+﻿using JobPayment.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPayment.Core.Entities
+{
+    public class Balance : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public double Coin { get; set; }
+        public double BonusCoin { get; set; }
+    }
+}
---
+++ src/Services/JobPayment/JobPayment.Core/Entities/OldPacket.cs
@@ -0,0 +1,17 @@
+﻿using JobPayment.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPayment.Core.Entities
+{
+    public class OldPacket : BaseEntity
+    {
+        public Packet Packet { get; set; }
+        public Guid PacketId { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
