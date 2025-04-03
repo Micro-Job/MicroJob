5bee488c4edfde488717cd44a58e1dc2d72fef44
LamanMammadly <lamanmammadly@gmail.com>
2025-04-03 19:08:33 +0400

    AddPublishEndpoint

--- src/Services/Job/JobCompany/JobCompany.Business/Services/ManageService/ManageService.cs
+++ src/Services/Job/JobCompany/JobCompany.Business/Services/ManageService/ManageService.cs
@@ -4,14 +4,16 @@ using JobCompany.Business.Exceptions.Common;
 using JobCompany.Business.Exceptions.VacancyExceptions;
 using JobCompany.Core.Entites;
 using JobCompany.DAL.Contexts;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
+using Shared.Events;
 using SharedLibrary.Enums;
 using SharedLibrary.Helpers;
 using SharedLibrary.HelperServices.Current;
 
 namespace JobCompany.Business.Services.ManageService;
 
-public class ManageService(JobCompanyDbContext _context,ICurrentUser _user) : IManageService
+public class ManageService(JobCompanyDbContext _context,ICurrentUser _user, IPublishEndpoint _publishEndpoint) : IManageService
 {
     private bool HasRole(string roleName)
     {
@@ -43,12 +45,30 @@ public class ManageService(JobCompanyDbContext _context,ICurrentUser _user) : IM
         }
         var vacancyGuid = Guid.Parse(dto.VacancyId);
         var vacancyCommenGuid = Guid.Parse(dto.VacancyCommentId);
-        var vacancy = await _context.Vacancies.Where(v => v.Id == vacancyGuid).FirstOrDefaultAsync()
+        var vacancy = await _context.Vacancies
+            .Include(v => v.Applications)
+            .Where(v => v.Id == vacancyGuid).FirstOrDefaultAsync()
             ?? throw new NotFoundException<Vacancy>(MessageHelper.GetMessage("NOT_FOUND"));
 
+        var appliedUserIds = vacancy.Applications.Select(a => a.UserId).ToList();
+
+        if (vacancy.Applications.Any())
+        {
+            _context.Applications.RemoveRange(vacancy.Applications);
+        }
         vacancy.VacancyCommentId = vacancyCommenGuid;
         vacancy.VacancyStatus = SharedLibrary.Enums.VacancyStatus.Reject;
         await _context.SaveChangesAsync();
+
+        await _publishEndpoint.Publish(
+            new VacancyUpdatedEvent
+            {
+                InformationId = vacancyGuid,
+                SenderId = (Guid)_user.UserGuid,
+                UserIds = appliedUserIds,
+                InformationName = vacancy.Title,
+            }
+        );
     }
 
     public async Task ToggleBlockVacancyStatusAsync(VacancyStatusUpdateDto dto)
