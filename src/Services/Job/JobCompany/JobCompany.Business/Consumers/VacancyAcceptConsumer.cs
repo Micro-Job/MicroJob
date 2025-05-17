259ffda8d77155ef9375b575dfe2385a42ec72d8
Brazencobra <elxan.memmedli2003@gmail.com>
2025-05-17 12:29:02 +0400

    FixedVacancyUpdatePaymentProblem

--- src/Services/Job/JobCompany/JobCompany.Business/Consumers/VacancyAcceptConsumer.cs
+++ src/Services/Job/JobCompany/JobCompany.Business/Consumers/VacancyAcceptConsumer.cs
@@ -16,7 +16,6 @@ namespace JobCompany.Business.Consumers
 {
     public class VacancyAcceptConsumer(JobCompanyDbContext _context, IRequestClient<CheckBalanceRequest> _balanceRequest, IPublishEndpoint _publishEndpoint, INotificationService _notificationService, IConfiguration _configuration) : IConsumer<VacancyAcceptEvent>
     {
-        //TODO : burada update olunmun vakansiyani accept ederken problemler var eger vaxti kecmiyibse odemeli deyil ve s kimi seyler nezere alinmayib
         public async Task Consume(ConsumeContext<VacancyAcceptEvent> context)
         {
             var vacancyGuid = Guid.Parse(context.Message.vacancyId);
@@ -24,28 +23,15 @@ namespace JobCompany.Business.Consumers
             var vacancy = await _context.Vacancies.Include(x => x.VacancySkills).Include(x => x.Company).FirstOrDefaultAsync(v => v.Id == vacancyGuid)
                 ?? throw new NotFoundException<Vacancy>(MessageHelper.GetMessage("NOT_FOUND"));
 
-            var balanceResponse = await _balanceRequest.GetResponse<CheckBalanceResponse>(new CheckBalanceRequest
-            {
-                InformationType = InformationType.Vacancy,
-                UserId = vacancy.Company.UserId
-            });
-
-            if ((vacancy.VacancyStatus == VacancyStatus.Pending || 
-                vacancy.VacancyStatus == VacancyStatus.Update || 
-                vacancy.VacancyStatus == VacancyStatus.PendingActive) && 
+            if ((vacancy.VacancyStatus == VacancyStatus.Pending ||
+                vacancy.VacancyStatus == VacancyStatus.Update ||
+                vacancy.VacancyStatus == VacancyStatus.PendingActive) &&
                 vacancy.EndDate >= DateTime.Now)
             {
-                if (balanceResponse.Message.HasEnoughBalance)
+                //TODO : bu hissede kod oxunaqligi itib yeniden daha seliqeli yazilmalidir
+                if (vacancy.VacancyStatus == VacancyStatus.Update && vacancy.PaymentDate > DateTime.Now)
                 {
-                    //await _publishEndpoint.Publish(new PayEvent
-                    //{
-                    //    InformationId = vacancyGuid,
-                    //    InformationType = InformationType.Vacancy,
-                    //    UserId = vacancy.Company.UserId
-                    //});
-
-                    vacancy.PaymentDate = DateTime.Now.AddDays(1);
-
+                    vacancy.VacancyStatus = VacancyStatus.Active;
                     await _notificationService.CreateNotificationAsync(new CreateNotificationDto
                     {
                         SenderId = null,
@@ -54,57 +40,88 @@ namespace JobCompany.Business.Consumers
                         InformationName = vacancy.Title,
                         ReceiverId = (Guid)vacancy.CompanyId
                     });
-                    if (vacancy.VacancyStatus == VacancyStatus.Update)
-                    {
-                        var appliedUserIds = await _context.Applications
-                         .Where(a => !a.IsDeleted && a.VacancyId == vacancyGuid)
-                         .Select(a => a.UserId) 
-                         .ToListAsync();
-
-                        await _publishEndpoint.Publish(new NotificationToUserEvent
-                        {
-                            SenderId = vacancy.Company.UserId,
-                            SenderName = vacancy.Company.CompanyName,
-                            SenderImage = $"{_configuration["JobCompany:BaseUrl"]}{vacancy.Company.CompanyLogo}",
-                            InformationId = vacancyGuid,
-                            NotificationType = NotificationType.VacancyUpdate,
-                            InformationName = vacancy.Title,
-                            ReceiverIds = appliedUserIds
-                        });
-                    }
-                    if (vacancy.VacancySkills != null && vacancy.VacancyStatus == VacancyStatus.Pending)
-                    {
-                        await _publishEndpoint.Publish(
-                            new VacancyCreatedEvent
-                            {
-                                SenderId = vacancy.Company.UserId,
-                                SkillIds = vacancy.VacancySkills.Select(x => x.SkillId).ToList(),
-                                InformationId = vacancy.Id,
-                                InformatioName = vacancy.Title,
-                            }
-                        );
-                    }
-                    vacancy.VacancyStatus = VacancyStatus.Active;
                 }
                 else
                 {
-                    vacancy.VacancyStatus = VacancyStatus.PendingActive;
-
-                    await _notificationService.CreateNotificationAsync(new CreateNotificationDto
+                    var balanceResponse = await _balanceRequest.GetResponse<CheckBalanceResponse>(new CheckBalanceRequest
                     {
-                        SenderId = null,
-                        InformationId = vacancyGuid,
-                        NotificationType = NotificationType.VacancyPendingActive,
-                        InformationName = vacancy.Title,
-                        ReceiverId = (Guid)vacancy.CompanyId
+                        InformationType = InformationType.Vacancy,
+                        UserId = vacancy.Company.UserId
                     });
+
+                    if (balanceResponse.Message.HasEnoughBalance)
+                    {
+                        await _publishEndpoint.Publish(new PayEvent
+                        {
+                            InformationId = vacancyGuid,
+                            InformationType = InformationType.Vacancy,
+                            UserId = vacancy.Company.UserId
+                        });
+
+                        vacancy.PaymentDate = DateTime.Now.AddDays(1);
+
+                        await _notificationService.CreateNotificationAsync(new CreateNotificationDto
+                        {
+                            SenderId = null,
+                            InformationId = vacancyGuid,
+                            NotificationType = NotificationType.VacancyAccept,
+                            InformationName = vacancy.Title,
+                            ReceiverId = (Guid)vacancy.CompanyId
+                        });
+                        if (vacancy.VacancyStatus == VacancyStatus.Update)
+                        {
+                            var appliedUserIds = await _context.Applications
+                             .Where(a => !a.IsDeleted && a.VacancyId == vacancyGuid)
+                             .Select(a => a.UserId)
+                             .ToListAsync();
+
+                            await _publishEndpoint.Publish(new NotificationToUserEvent
+                            {
+                                SenderId = vacancy.Company.UserId,
+                                SenderName = vacancy.Company.CompanyName,
+                                SenderImage = $"{_configuration["JobCompany:BaseUrl"]}{vacancy.Company.CompanyLogo}",
+                                InformationId = vacancyGuid,
+                                NotificationType = NotificationType.VacancyUpdate,
+                                InformationName = vacancy.Title,
+                                ReceiverIds = appliedUserIds
+                            });
+                        }
+                        if (vacancy.VacancySkills != null && vacancy.VacancyStatus == VacancyStatus.Pending)
+                        {
+                            await _publishEndpoint.Publish(
+                                new VacancyCreatedEvent
+                                {
+                                    SenderId = vacancy.Company.UserId,
+                                    SkillIds = vacancy.VacancySkills.Select(x => x.SkillId).ToList(),
+                                    InformationId = vacancy.Id,
+                                    InformatioName = vacancy.Title,
+                                }
+                            );
+                        }
+                        vacancy.VacancyStatus = VacancyStatus.Active;
+                    }
+                    else
+                    {
+                        vacancy.VacancyStatus = VacancyStatus.PendingActive;
+
+                        await _publishEndpoint.Publish(new PayEvent
+                        {
+                            InformationId = vacancyGuid,
+                            InformationType = InformationType.Vacancy,
+                            UserId = vacancy.Company.UserId
+                        });
+
+                        await _notificationService.CreateNotificationAsync(new CreateNotificationDto
+                        {
+                            SenderId = null,
+                            InformationId = vacancyGuid,
+                            NotificationType = NotificationType.VacancyPendingActive,
+                            InformationName = vacancy.Title,
+                            ReceiverId = (Guid)vacancy.CompanyId
+                        });
+                    }
+
                 }
-                await _publishEndpoint.Publish(new PayEvent
-                {
-                    InformationId = vacancyGuid,
-                    InformationType = InformationType.Vacancy,
-                    UserId = vacancy.Company.UserId
-                });
             }
 
             await _context.SaveChangesAsync();
